32d1dffb3b60de22dff43767fb108b77c2ae5748
M Brabu <brabumurugan18@gmail.com>
2026-08-05 19:41:50 +0530

    log out rm

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ()=>{
             <div className="navbar">
             <h2 className='navtitle'><img id="logo" src={logo} alt="CR" />CodeLens</h2>
             <div className='navbtns'>
-                <button className='navbtn2'>logout</button>
+                {/* <button className='navbtn2'>logout</button> */}
             </div>
             </div>
         </div>
